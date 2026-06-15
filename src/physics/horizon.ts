6a894fb9b18d4fcf63a7d6e7dc417fafa833da7d
Nathan Segura <nathan.es@icloud.com>
2026-06-15 09:57:48 -0700

    Added NaN check to apparentHorizonRadiusFromMass

--- src/physics/horizon.ts
+++ src/physics/horizon.ts
@@ -1,6 +1,11 @@
-export function apparentHorizonRadiusFromMass(mass:number):number{
-    if(mass < 0){
+export function apparentHorizonRadiusFromMass(mass: number): number {
+    if (mass < 0) {
         throw new Error("Mass less than 0");
     }
-    return mass*2;
+
+	if (mass !== mass) {
+		throw new Error("Mass is NaN");
+	}
+
+    return mass * 2;
 }
