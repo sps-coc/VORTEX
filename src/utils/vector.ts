d98af3576ba2e7484220cd17a2a47ffae3126720
Nathan Segura <nathan.es@icloud.com>
2026-06-15 10:01:56 -0700

    Imported Vector2 from simulation/types.ts

--- src/utils/vector.ts
+++ src/utils/vector.ts
@@ -1,30 +1,27 @@
-export interface Vector {
-	x: number;
-	y: number;
-}
+import type { Vector2 } from "../simulation";
 
-export function distance(vect1: Vector, vect2: Vector): number {
+export function distance(vect1: Vector2, vect2: Vector2): number {
 	const xDistance: number = vect2.x - vect1.x;
 	const yDistance: number = vect2.y - vect1.y;
 
 	return Math.sqrt(xDistance**2 + yDistance**2);
 }
 
-export function add(vect1: Vector, vect2: Vector): Vector {
+export function add(vect1: Vector2, vect2: Vector2): Vector2 {
 	return {
 		x: vect1.x + vect2.x,
 		y: vect1.y + vect2.y
 	};
 }
 
-export function scale(vect: Vector, scalar: number): Vector {
+export function scale(vect: Vector2, scalar: number): Vector2 {
 	return {
 		x: scalar * vect.x,
 		y: scalar * vect.y
 	};
 }
 
-export function normalize(vect: Vector): Vector {
+export function normalize(vect: Vector2): Vector2 {
 	const mag: number = Math.sqrt(vect.x**2 + vect.y**2);
 
 	if (mag == 0){
